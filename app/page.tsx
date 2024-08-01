e76ef37c8687d4d72596f38cb9af69f7c93170f4
ZXY101 <xzxy101x@gmail.com>
2024-08-01 16:31:31 +0200

    hmmm

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+
+export default async function Home() {
+
+  const hmm = await getData()
+  console.log('hmm', hmm);
+
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h2>{hmm?.title}</h2>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
